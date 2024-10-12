f3ba1adc47f8ef830d7151ab619254b5e3f3c132
ROHITH <rohith171801@gmail.com>
2024-10-12 11:05:46 +0530

    if-else-if

---
+++ CWH_PY/P3.py
@@ -0,0 +1,10 @@
+# Conditional statements
+a = int(input("Enter your age : "))
+if(a >= 45):
+    print("You can drive and work")
+elif(a >= 18 ):
+    print("You can drive")
+else:
+    print("You can't drive")
+    
+# elif is simply if-else-if
