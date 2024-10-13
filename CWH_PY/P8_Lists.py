bcd8f23bf0c5100e91833be7f4c71a2d1e9dafd9
ROHITH <rohith171801@gmail.com>
2024-10-13 17:16:26 +0530

    ListMethods

--- CWH_PY/P8_Lists.py
+++ CWH_PY/P8_Lists.py
@@ -22,3 +22,31 @@ lst1 = [i * i for i in range(10)]
 print(lst1)
 lst1 = [i * i for i in range(10) if i % 2 == 0]
 print(lst1)
+
+# List Methods
+l = [11,2,5,6,4,1]
+print(l)
+l.append(7)
+print(l)
+l.sort()
+print(l)
+l.sort(reverse=True)
+print(l)
+l.reverse()
+print(l.index(2))
+print(l.count(2))
+print(l)
+m = l
+# m[0] = 0 # l also changes
+# print(l)
+m = l.copy()
+m[0] = 0 # l doesn't change
+print(l)
+l.insert(2,900)
+print(l)
+m = [100,900,300]
+l.extend(m)
+print(l)
+print(m)
+k = l + m
+print(k)
