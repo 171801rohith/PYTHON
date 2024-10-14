2459badde606131c7eb2bfae8fc917784b4a7ef6
ROHITH <rohith171801@gmail.com>
2024-10-14 15:24:07 +0530

    basic oops

---
+++ OOPSConcepts.py
@@ -0,0 +1,18 @@
+class Dog:  # Brackets are optional
+    def __init__(self, name):
+        # self ='this' in java
+        print('Hey bro aadithya is the "GreatAssGaint"', name)
+        self.dog_name = name
+
+    def bark(self):
+        print(self.dog_name, "says, Wooff!!")
+    
+    def walk(self, location):
+        print(self.dog_name,"is walking towards",location)
+
+
+#  Funciton when defined inside a class then they are called 'Methods'
+
+dog1 = Dog("John")  # Objects/Instances of class Dog
+dog1.bark()
+dog1.walk("West")
