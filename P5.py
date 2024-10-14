3cd82ac7220116c97018b40cd09ad3c2d3195a8a
ROHITH <rohith171801@gmail.com>
2024-10-14 15:53:23 +0530

    simOOp

---
+++ P5.py
@@ -0,0 +1,10 @@
+# Step 1: Define a class called car
+# Step 2: In the constructor define 1: Make of car 2: Model of car
+# Step 3: Print the maker and model
+
+class car():
+    def __init__(self, maker, model):
+        print("1. Maker of the car :", maker)
+        print("2. Model of the car :", model)
+
+McQueen = car('Audi', 'R8')
