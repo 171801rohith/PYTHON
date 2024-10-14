fcc9e24d98e480dc146a6677d88d11dcc23a7451
ROHITH <rohith171801@gmail.com>
2024-10-14 16:04:30 +0530

    attributes

--- P5.py
+++ P5.py
@@ -1,10 +1,13 @@
 # Step 1: Define a class called car
-# Step 2: In the constructor define 1: Make of car 2: Model of car
-# Step 3: Print the maker and model
+# Step 2: Define a class attribute called 'wheels
+# Step 3: In the constructor define 1: Make of car 2: Model of car
+# Step 4: Print the maker and model
 
 class car():
+    wheels = 4
     def __init__(self, maker, model):
         print("1. Maker of the car :", maker)
         print("2. Model of the car :", model)
 
 McQueen = car('Audi', 'R8')
+print(McQueen.wheels)
