9d51c02d37f0a0467dd2b378b1bd31069dee255c
ROHITH <rohith171801@gmail.com>
2024-10-14 16:05:11 +0530

    simOops

--- OOPSConcepts.py
+++ OOPSConcepts.py
@@ -1,5 +1,8 @@
 class Dog:  # Brackets are optional
     # One parameter is compalsory so we use 'self' everytime when we define a Method. Don't ever miss 'self'
+
+    noOfLegs = 4  # Attribute creation
+
     def __init__(self, name):
         # self ='this' in java
         print('Hey bro aadithya is the "GreatAssGaint"', name)
@@ -7,9 +10,9 @@ class Dog:  # Brackets are optional
 
     def bark(self):
         print(self.dog_name, "says, Wooff!!")
-    
-    def walk(self, location = 'park'):
-        print(self.dog_name,"is walking towards",location)
+
+    def walk(self, location="park"):
+        print(self.dog_name, "is walking towards", location)
 
 
 #  Funciton when defined inside a class then they are called 'Methods'
@@ -18,3 +21,4 @@ dog1 = Dog("John")  # Objects/Instances of class Dog
 dog1.bark()
 dog1.walk("a car wheel")
 dog1.walk()
+print(dog1.noOfLegs)
