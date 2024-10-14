8d37527b17d861f6abc65de3af965f30ddae2f91
ROHITH <rohith171801@gmail.com>
2024-10-14 14:58:46 +0530

    Functions

--- CWH_PY/P7_Funcs.py
+++ CWH_PY/P7_Funcs.py
@@ -1,18 +1,22 @@
 #  User-Define-Functions in Python
 
+
 def CalcMean(a, b):
-    mean = (a * b)/(a + b)
+    mean = (a * b) / (a + b)
     print(mean)
 
+
 def CalcGreat(a, b):
-    if(a > b):
-        print(a,"is greater")
+    if a > b:
+        print(a, "is greater")
     else:
-        print(b,"is greater")
-        
+        print(b, "is greater")
+
+
 def CalcSomething():
-    pass
-        
+    pass  # Just Ignores the function and goes to next line
+
+
 a = 4
 b = 10
 CalcMean(a, b)
@@ -25,21 +29,26 @@ CalcGreat(c, d)
 
 # Function Arguments and Return statements
 
+
 # Default Arguments
-def avg(a = 9, b = 8):
-    print("Avg is", (a + b)/2)
+def avg(a=9, b=8):
+    print("Avg is", (a + b) / 2)
+
 
 avg()
-avg(12, 30) # It will overwrite 9 and 8 
-avg(a = 30) # It will overwrite only 9 we can also do vice-versa
+avg(12, 30)  # It will overwrite 9 and 8
+avg(a=30)  # It will overwrite only 9 we can also do vice-versa
+
 
 # Required Arguments
 def avg(a, b):
-    print("Avg is", (a + b)/2)
+    print("Avg is", (a + b) / 2)
+
 
 # avg() # Error
 # avg(a = 30) #Error
-avg(12, 30) # We have to pass to arguments in Required Arguments 
+avg(12, 30)  # We have to pass to arguments in Required Arguments
+
 
 # Variable Length Arguments
 def avg(*nums):
@@ -48,8 +57,10 @@ def avg(*nums):
     for i in nums:
         sum += i
     print("avg is", sum / len(nums))
-    
-avg(4, 5, 5, 7, 9) # We can give any number of args
+
+
+avg(4, 5, 5, 7, 9)  # We can give any number of args
+
 
 # Returning values
 def avg(*nums):
@@ -58,6 +69,7 @@ def avg(*nums):
     for i in nums:
         sum += i
     return sum / len(nums)
-    
-a = avg(4, 5, 5, 7, 9) 
+
+
+a = avg(4, 5, 5, 7, 9)
 print("avg is", a)
