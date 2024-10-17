da531e81e13e908703ee8d07e6d1c6e69addb4a5
ROHITH <rohith171801@gmail.com>
2024-10-17 15:50:52 +0530

    exTry

---
+++ P7.py
@@ -0,0 +1,27 @@
+# Step 1: Create a method
+# Step 2: Declare 2 variable
+# Step 3: Create Try Catch
+# Step 4: Inside try to divide num1/num2
+# Step 5: Create except, else and finally
+# Step 6: handle ZeroDivisionError, TypeError
+# Step 7: Test cases (10 ,2), (10,0), (10, 'a')
+
+
+num1 = 10
+# num2 = 'a'
+# num2 = 0
+num2 = 2
+try:
+    result = num1 / num2
+
+except TypeError:
+    print("Error invalid input type. Enter a integer")
+
+except ZeroDivisionError:
+    print("Error invalid denominator. Can't divide by zero")
+
+else:
+    print("The result is :",result)
+
+finally: 
+    print("Execution of the divide_numbers is complete")
