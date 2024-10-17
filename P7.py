090a7b3c0d436c54568a73e9b961a67111f3dc38
ROHITH <rohith171801@gmail.com>
2024-10-17 15:52:50 +0530

    updated

--- P7.py
+++ P7.py
@@ -7,21 +7,25 @@
 # Step 7: Test cases (10 ,2), (10,0), (10, 'a')
 
 
+def TryCatch(num1, num2):
+    try:
+        result = num1 / num2
+
+    except TypeError:
+        print("Error invalid input type. Enter a integer")
+
+    except ZeroDivisionError:
+        print("Error invalid denominator. Can't divide by zero")
+
+    else:
+        print("The result is :", result)
+
+    finally:
+        print("Execution of the divide_numbers is complete")
+
+
 num1 = 10
 # num2 = 'a'
 # num2 = 0
 num2 = 2
-try:
-    result = num1 / num2
-
-except TypeError:
-    print("Error invalid input type. Enter a integer")
-
-except ZeroDivisionError:
-    print("Error invalid denominator. Can't divide by zero")
-
-else:
-    print("The result is :",result)
-
-finally: 
-    print("Execution of the divide_numbers is complete")
+TryCatch(num1, num2)
