a48fde87ade8b545653afe65ce0c6472d49b523b
ROHITH <rohith171801@gmail.com>
2024-10-30 20:25:46 +0530

    Loop with Else

---
+++ CWH_PY/P16_ForElse.py
@@ -0,0 +1,13 @@
+# For loop with else
+
+for i in range(5):
+    print(i)
+else:
+    print("No i found")
+
+for i in range(9):
+    if i == 4:
+        break;
+    print(i)
+else :
+    print("No i")
