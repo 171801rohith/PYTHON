5fee528630cfe0d4d0a36ae9b04389908d955fe8
ROHITH <rohith171801@gmail.com>
2024-11-03 19:04:02 +0530

    Exception Handling

--- CWH_PY/P17_ExceptionHandling.py
+++ CWH_PY/P17_ExceptionHandling.py
@@ -1,28 +1,28 @@
 # Error or Exception Handling
 # Try catch
 
-# a = input("Enter a number :")
-# print(f"Multiplication table of {a} :")
-# try:
-#     for i in range(1, 11):
-#         print(f"{a} x {i} =", int(a) * i)
-# except Exception as e:
-#     print("Sorry, something went wrong 404 error")
-#     print(e)
+a = input("Enter a number :")
+print(f"Multiplication table of {a} :")
+try:
+    for i in range(1, 11):
+        print(f"{a} x {i} =", int(a) * i)
+except Exception as e:
+    print("Sorry, something went wrong 404 error")
+    print(e)
 
 
-# print("Some important Lines of Code")
-# print("End of Code")
+print("Some important Lines of Code")
+print("End of Code")
 
-# try:
-#     print(4 / 0)
-# except Exception as e:
-#     print("Error Occurred:", e)
+try:
+    print(4 / 0)
+except Exception as e:
+    print("Error Occurred:", e)
 
-# try:
-#     print(4 / 0)
-# except Exception:
-#     print("Error Occurred:")
+try:
+    print(4 / 0)
+except Exception:
+    print("Error Occurred:")
 
 # Some Errors
 # Value error
@@ -56,3 +56,12 @@ def fun2():
 
 print(fun1())
 print(fun2())
+
+# Custom error
+
+a = 3 # A num btwn 5 - 9
+# a = 6 # A num btwn 5 - 9
+if a < 5 or a > 9:
+    raise ValueError("Enter a num between 5 and 9")
+else:
+    print(a)
