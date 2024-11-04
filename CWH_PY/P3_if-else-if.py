651b2915a21bcf281880695ed7713e4ee8039e96
ROHITH <rohith171801@gmail.com>
2024-11-04 18:56:34 +0530

    Short Hand IF

--- CWH_PY/P3_if-else-if.py
+++ CWH_PY/P3_if-else-if.py
@@ -8,3 +8,13 @@ else:
     print("You can't drive")
     
 # elif is simply if-else-if
+
+# Short Hand If Else (Similar to Ternary Operator)
+    # value_if_true if condition else value_if_false
+a = 248
+b = 234
+print("A") if a > b else print("=") if a == b else print("B")
+
+print(9) if a < b else ""
+c = 9 if a < b else 0
+print(c)
