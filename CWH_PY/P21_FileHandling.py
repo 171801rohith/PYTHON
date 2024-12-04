c981609c10ac9eec49b469cb18a926dd5ebf2e32
ROHITH <rohith171801@gmail.com>
2024-12-04 21:55:09 +0530

    Read and wrilte lines Seek and tell

--- CWH_PY/P21_FileHandling.py
+++ CWH_PY/P21_FileHandling.py
@@ -17,3 +17,34 @@ f.close()
 
 with open("C:/Users/rohit/Desktop/PYTHON/CWH_PY/myfile.txt", "a") as f:
     f.write("Broooo!")
+
+f.close()
+
+# ReadLines method
+f = open("C:/Users/rohit/Desktop/PYTHON/CWH_PY/mymarks.txt", "r")
+while True:
+    line = f.readline()
+    if not line:
+        break
+    m0 = line.split(",")[0]
+    m1 = line.split(",")[1]
+    m2 = line.split(",")[2]
+    print(line)
+    print(f"M0 : {m0}\tM1 : {m1}\tM2 : {m2}")
+
+f.close()
+
+# WriteLines method
+f = open("C:/Users/rohit/Desktop/PYTHON/CWH_PY/myfile.txt", "w")
+lines = ["line1\n", "line2\n", "line3\n"]
+f.writelines(lines)
+f.truncate(8)
+f.close()
+
+# Seek & Tell method
+with open("C:/Users/rohit/Desktop/PYTHON/CWH_PY/myfile.txt", "r") as f:
+    print(type(f))
+    f.seek(10)
+    print(f.tell())
+    data = f.read(5)
+    print(data)
