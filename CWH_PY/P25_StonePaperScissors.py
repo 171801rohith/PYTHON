755071234ca437a3d425660128bc103ee129a54e
ROHITH <rohith171801@gmail.com>
2024-12-10 22:45:48 +0530

    StonePaperScissors

---
+++ CWH_PY/P25_StonePaperScissors.py
@@ -0,0 +1,21 @@
+# Stone Paper Scissors
+import random
+
+def check_winner(user, computer):
+    if user == computer:
+        return "It's a Tie !! -_-"
+    elif (user == 1 and computer == 2) or (user == 2 and computer == 3) or (user == 3 and computer == 1):
+        return "You Win !! :)"
+    else :
+        return "You Lose !! :("
+
+while True:
+    user = int(input("Enter your choice:    1. Snake     2. Water    3. Gun   4. Exit : "))
+    computer = int(random.uniform(1, 4))
+    if user == 4:
+        print("Exiting.....")
+        break
+    else:
+        print("Computer choose ", computer)
+        print(check_winner(user, computer))
+    
