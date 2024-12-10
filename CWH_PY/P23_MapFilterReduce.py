579a97007814719175490abb157e68ab85fe3660
ROHITH <rohith171801@gmail.com>
2024-12-10 22:09:41 +0530

    MAP FILTER REDUCE

---
+++ CWH_PY/P23_MapFilterReduce.py
@@ -0,0 +1,29 @@
+# Map
+# Filter
+# Reduce
+from functools import reduce
+
+cube = lambda x: x * x * x
+sqr = lambda x: x * x 
+
+l = [1, 2, 3, 4, 5, 6, 7]
+nl = []
+# for i in l:
+#     nl.append(cube(i))
+
+# MAP
+# map(cube, l) returns a object
+nl =list(map(cube, l))
+print(nl)
+nl =list(map(sqr, l))
+print(nl)
+
+# FILTER
+# filter(filter_func, l) returns a object
+filter_func = lambda x: x > 4
+nl = list(filter(filter_func, l))
+print(nl)
+
+# REDUCE
+sum = reduce(lambda x, y: x + y, l)
+print(sum)
