f2547fd9e3b1030e249b785ebdb3715ab1e2db98
ROHITH <rohith171801@gmail.com>
2025-01-07 10:52:18 +0530

    Win32API

---
+++ CWH_PY/P40_win32API.py
@@ -0,0 +1,24 @@
+# Write a program to pronounce t ist of names using win32 API.
+# If you are given a list l as follows:
+# l = ["Rahul", "Nishant", "Harry"]
+# Your program should pronouce:
+# Shoutout to Rahul
+# Shoutout to Nishant
+
+import win32com.client as win32
+
+class ShoutOut:
+    speaker = win32.Dispatch("SAPI.SpVoice")
+    def __init__(self, names):
+        self.names = names
+    
+    def pronounce(self):
+        for name in names:
+            shoutout = f"Shoutout to {name}"
+            self.speaker.speak(shoutout)
+
+
+
+names = ["Rohith", "Shek", "Aadithya", "Karthik"]
+shout = ShoutOut(names)
+shout.pronounce()
