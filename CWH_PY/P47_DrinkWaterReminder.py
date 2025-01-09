7ac096a1d0f195ab8fe9958574b5dcfa693f47c4
ROHITH <rohith171801@gmail.com>
2025-01-09 16:57:25 +0530

    simple drink water reminder everyhour

--- CWH_PY/P47_DrinkWaterReminder.py
+++ CWH_PY/P47_DrinkWaterReminder.py
@@ -4,9 +4,9 @@ import time
 def speak_out():
     speaker = win32.Dispatch("SAPI.Spvoice")
     while True:
-        sec = int(time.strftime("%S", time.localtime()))
-        print(sec)
-        if sec == 59:
+        min = int(time.strftime("%M", time.localtime()))
+        print(min)
+        if min == 59:
             print("Hello")
             speaker.speak("It's time to drink water.")
             time.sleep(1)
