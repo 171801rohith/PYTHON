28d019545d3c06017e1ee97351ec8c45dc0164f2
ROHITH <rohith171801@gmail.com>
2025-01-10 12:35:27 +0530

    # Display Notifications

---
+++ CWH_PY/P52_win11ToastMod.py
@@ -0,0 +1,7 @@
+# Display Notifications
+
+import os
+from win10toast import ToastNotifier
+
+toast = ToastNotifier()
+toast.show_toast("Welcome to Python ", "This is cool!", duration=4, icon_path= None)
