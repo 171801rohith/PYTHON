a009968b7e151413f3e08b3ed341421a6827e669
ROHITH <rohith171801@gmail.com>
2025-02-05 17:57:04 +0530

    FlaskWtf

---
+++ myFlask/FlaskWtf/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "Hello "
+
+
+class NameForm(FlaskForm):
+    name = StringField("Name :", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("layout.html", name=name, form=form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
