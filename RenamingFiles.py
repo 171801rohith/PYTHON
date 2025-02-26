c86b6683e29bbf8db887496170e0579aa726d886
ROHITH <rohith171801@gmail.com>
2025-02-26 12:14:02 +0530

    commit

--- RenamingFiles.py
+++ RenamingFiles.py
@@ -1,18 +1,18 @@
 import os
 
-path = r"R:\MOVIES\S02"
+path = r"R:\MOVIES\DAIMA"
 
 episodes = os.listdir(path)
 
 for episode in episodes:
     print(episode)
 
-print()
-i = 1
-for episode in episodes:
-    oldpath = os.path.join(path, episode)
-    newname = f"E0{i}.mkv"
-    newpath = os.path.join(path, newname)
-    os.rename(oldpath, newpath)
-    print(episode, "->", newname)
-    i += 1
+# print()
+# i = 1
+# for episode in episodes:
+#     oldpath = os.path.join(path, episode)
+#     newname = f"E0{i}.mkv"
+#     newpath = os.path.join(path, newname)
+#     os.rename(oldpath, newpath)
+#     print(episode, "->", newname)
+#     i += 1
